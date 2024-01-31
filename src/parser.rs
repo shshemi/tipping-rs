6325dc99854fb0e863492d983b4a96c0b6a76be1
Shayan Hashemi <shshemi@gmail.com>
2024-02-01 01:46:04 +0200

    add doc comments for public Parser

--- src/parser.rs
+++ src/parser.rs
@@ -23,6 +23,7 @@ type Masks = HashMap<String, String>;
 pub struct NoCompute;
 pub struct Compute;
 
+/// Tipping (Token Interdependency Parsing) log parser
 #[derive(Debug, Clone)]
 pub struct Parser<Templates = NoCompute, Masks = NoCompute> {
     threshold: f32,
@@ -42,6 +43,7 @@ impl Default for Parser {
     }
 }
 impl Parser {
+    /// Initiates a new Parser with default parameters
     pub fn new() -> Self {
         Parser {
             threshold: 0.5,
@@ -56,42 +58,54 @@ impl Parser {
         }
     }
 
+    /// Sets `value` as threshold. The threshold determines if an interdependency
+    /// link should be considered during key node computation or not. The
+    /// threshod should be `0 <= threshold <= 1`.
     #[must_use]
     pub fn with_threshold(mut self, value: f32) -> Self {
+        assert!(0.0 <= value);
+        assert!(value <= 1.0);
         self.threshold = value;
         self
     }
 
+    /// Sets `value` as spcial white regexes. White regexes are never parameterized.
     #[must_use]
     pub fn with_special_whites(mut self, value: Vec<Regex>) -> Self {
         self.special_whites = value;
         self
     }
 
+    /// Sets `value` as special black regexes. Black regexes are always parameterized.
     #[must_use]
     pub fn with_special_blacks(mut self, value: Vec<Regex>) -> Self {
         self.special_blacks = value;
         self
     }
 
+    /// Sets `value` as symbols. Symbolds are characters that are used alongside the
+    /// white spaces to split string during tokenization.
     #[must_use]
     pub fn with_symbols(mut self, value: HashSet<char>) -> Self {
         self.symbols = value;
         self
     }
 
+    /// The `value` determines if alphabetic tokens should be used during key node computation.
     #[must_use]
     pub fn with_filter_alphabetic(mut self, value: bool) -> Self {
         self.filter_alphabetic = value;
         self
     }
 
+    /// The `value` determines if numeric tokens should be used during key node computation.
     #[must_use]
     pub fn with_filter_numeric(mut self, value: bool) -> Self {
         self.filter_numeric = value;
         self
     }
 
+    /// The `value` determines if impure tokens should be used during key node computation.
     #[must_use]
     pub fn with_filter_impure(mut self, value: bool) -> Self {
         self.filter_impure = value;
@@ -100,6 +114,7 @@ impl Parser {
 }
 
 impl<T> Parser<NoCompute, T> {
+    // Add templates computation to the output
     #[must_use]
     pub fn compute_templates(self) -> Parser<Compute, T> {
         Parser::<Compute, T> {
@@ -117,6 +132,7 @@ impl<T> Parser<NoCompute, T> {
 }
 
 impl<T> Parser<T, NoCompute> {
+    // Add parameter mask computation to the output
     #[must_use]
     pub fn compute_masks(self) -> Parser<T, Compute> {
         Parser::<T, Compute> {
@@ -134,6 +150,12 @@ impl<T> Parser<T, NoCompute> {
 }
 
 impl Parser<NoCompute, NoCompute> {
+    /// Parses the input `messages` and returns `Clusters`.
+    ///
+    /// - `Clusters`: A `Vec<Option<usize>>` representing potential cluster IDs. Each `Option<usize>`
+    ///   corresponds to the cluster ID of the message at the same index, or `None` if the message
+    ///   couldn't be parsed.
+    ///
     pub fn parse<Message: AsRef<str> + Sync>(self, messages: &[Message]) -> Clusters {
         let tokenizer = Tokenizer::new(self.special_whites, self.special_blacks, self.symbols);
         let filter = InterdependencyFilter::with(
@@ -157,6 +179,15 @@ impl Parser<NoCompute, NoCompute> {
 }
 
 impl Parser<Compute, NoCompute> {
+    /// Parses the input `messages` and returns `Clusters`, and `Templates`.
+    ///
+    /// - `Clusters`: A `Vec<Option<usize>>` representing potential cluster IDs. Each `Option<usize>`
+    ///   corresponds to the cluster ID of the message at the same index, or `None` if the message
+    ///   couldn't be parsed.
+    ///
+    /// - `Templates`: A `Vec<HashSet<String>>` where each set of templates is aligned with the
+    ///   corresponding cluster ID in the `Clusters` vector.
+    ///
     pub fn parse<Message: AsRef<str> + Sync>(self, messages: &[Message]) -> (Clusters, Templates) {
         let tokenizer = Tokenizer::new(self.special_whites, self.special_blacks, self.symbols);
         let filter = InterdependencyFilter::with(
@@ -194,6 +225,14 @@ impl Parser<Compute, NoCompute> {
 }
 
 impl Parser<NoCompute, Compute> {
+    /// Parses the input `messages` and returns `Clusters`,  `Masks`.
+    ///
+    /// - `Clusters`: A `Vec<Option<usize>>` representing potential cluster IDs. Each `Option<usize>`
+    ///   corresponds to the cluster ID of the message at the same index, or `None` if the message
+    ///   couldn't be parsed.
+    ///
+    /// - `Masks`: A table mapping each message to its parameter masks.
+    ///
     pub fn parse<Message: AsRef<str> + Sync>(self, messages: &[Message]) -> (Clusters, Masks) {
         let tokenizer = Tokenizer::new(self.special_whites, self.special_blacks, self.symbols);
         let filter = InterdependencyFilter::with(
@@ -231,6 +270,17 @@ impl Parser<NoCompute, Compute> {
 }
 
 impl Parser<Compute, Compute> {
+    /// Parses the input `messages` and returns `Clusters`, `Templates`, and `Masks`.
+    ///
+    /// - `Clusters`: A `Vec<Option<usize>>` representing potential cluster IDs. Each `Option<usize>`
+    ///   corresponds to the cluster ID of the message at the same index, or `None` if the message
+    ///   couldn't be parsed.
+    ///
+    /// - `Templates`: A `Vec<HashSet<String>>` where each set of templates is aligned with the
+    ///   corresponding cluster ID in the `Clusters` vector.
+    ///
+    /// - `Masks`: A table mapping each message to its parameter masks.
+    ///
     pub fn parse<Message: AsRef<str> + Sync>(
         self,
         messages: &[Message],
@@ -246,7 +296,8 @@ impl Parser<Compute, Compute> {
         let mut clus = vec![None; messages.len()];
         let mut temps = vec![HashSet::default(); groups.len()];
         let mut masks = HashMap::new();
-        groups.into_iter()
+        groups
+            .into_iter()
             .filter(|(key_toks, _)| !key_toks.is_empty())
             .enumerate()
             .for_each(|(cid, (_, indices))| {
